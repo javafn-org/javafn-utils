3074c279715102ec9826806b84be24ff118cb600
Ben Stringer <ben.stringer@nrlssc.navy.mil>
2026-09-08 13:38:51 -0500

    Added the RestartingThread class

---
+++ src/main/java/org/javafn/utils/RestartingThread.java
@@ -0,0 +1,44 @@
+package org.javafn.utils;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.function.BooleanSupplier;
+
+public final class RestartingThread extends Thread implements UncaughtExceptionHandler {
+
+	public interface ThreadContext {
+		Runnable runnable();
+		String threadName();
+		boolean isRunning();
+		void onThreadInstanceChanged(RestartingThread newInstance, Throwable cause);
+		void onThreadInstanceExited(Throwable cause);
+	}
+	public static abstract class ThreadContextImpl implements ThreadContext, Runnable {
+		private final String threadName;
+		private final BooleanSupplier isRunning;
+
+		public ThreadContextImpl(final String _threadName, final BooleanSupplier _isRunning ) {
+			threadName = _threadName;
+			isRunning = _isRunning;
+		}
+		@Override public Runnable runnable() { return this; }
+		@Override public String threadName() { return threadName; }
+		@Override public boolean isRunning() { return isRunning.getAsBoolean(); }
+	}
+
+	private final ThreadContext ctx;
+
+	public RestartingThread(final ThreadContext _ctx) {
+		super(_ctx.runnable(), _ctx.threadName());
+		ctx = _ctx;
+		setUncaughtExceptionHandler(this);
+		start();
+	}
+
+	@Override public void uncaughtException(final Thread t, final Throwable e) {
+		if (ctx.isRunning()) {
+			ctx.onThreadInstanceChanged(new RestartingThread(ctx), e);
+		} else {
+			ctx.onThreadInstanceExited(e);
+		}
+	}
+}
